ede3c4c6373503d33a8c50dba0e90ca99e3aab70
Quin Hoxie <qhoxie@gmail.com>
2009-10-09 23:50:05 -0700

    Whitespace changes

--- src/main/scala/Step.scala
+++ src/main/scala/Step.scala
@@ -11,7 +11,7 @@ object Step
 {
   type Params = Map[String, String]
   type Action = Any => String
-  
+
   val protocols = List("GET", "POST", "PUT", "DELETE")
 }
 import Step._
@@ -21,11 +21,11 @@ abstract class Step extends HttpServlet
   val Routes      = Map(protocols map (_ -> new HashSet[Route]): _*)
   val paramsMap   = new DynamicVariable[Params](null)
   var contentType = "text/html"
-  
+
   class Route(val path: String, val action: Action) {
     val pattern = """:\w+"""
     val names = new Regex(pattern) findAllIn path toList
-    val re = new Regex("^%s$" format path.replaceAll(pattern, "(.*?)"))      
+    val re = new Regex("^%s$" format path.replaceAll(pattern, "(.*?)"))
 
     def apply(realPath: String): Option[Params] =
       re findFirstMatchIn realPath map (x => Map(names zip x.subgroups : _*))
@@ -37,7 +37,7 @@ abstract class Step extends HttpServlet
     val realParams = new MapWrapper[String, Array[String]]() {
       def underlying = request.getParameterMap.asInstanceOf[java.util.Map[String,Array[String]]]
     }.map { case (k,v) => (k, v(0)) }
-    
+
     def isMatchingRoute(route: Route) = {
       def exec(args: Params) = {
         before _
@@ -45,22 +45,22 @@ abstract class Step extends HttpServlet
         paramsMap.withValue(args ++ realParams) {
           response.getWriter print route.action()
         }
-      } 
+      }
       //getPathInfo returns everything after the context path, so step will work if non-root
       route(request.getPathInfo) map exec isDefined
     }
-    
+
     if (Routes(request.getMethod) find isMatchingRoute isEmpty)
       response.getWriter println "Requesting %s but only have %s".format(request.getRequestURI, Routes)
   }
-  
+
   def params(name: String): String = paramsMap value name
   def before(fun: => Any) = fun
-  val List(get, post, put, delete) = protocols map routeSetter  
+  val List(get, post, put, delete) = protocols map routeSetter
 
   // functional programming means never having to repeat yourself
   private def routeSetter(protocol: String): (String) => (=> Any) => Unit = {
     def g(path: String, fun: => Any) = Routes(protocol) += new Route(path, x => fun.toString)
     (g _).curry
-  }  
+  }
 }
